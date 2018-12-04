1c7634c499dadd866213065be1283a316079377d
Susanna Brumm <susanna.brumm@gmail.com>
2018-12-04 16:38:00 -0500

    yet another change

--- module5/js/script.js
+++ module5/js/script.js
@@ -115,7 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(category['short_name']);
+      var homeHtmlToInsertIntoMainPage = insertProperty(categories['short_name']);
 
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
